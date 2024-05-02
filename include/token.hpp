46171035c08189c9f8cfe86e842032fb68d55fa8
Supreet <singh.supreet14@gmail.com>
2024-05-02 22:32:38 +0530

    Parsing implementation in token

--- include/token.hpp
+++ include/token.hpp
@@ -1,62 +1,123 @@
 #pragma once
+#include <map>
+#include <set>
 #include <string>
-#include <iostream>
-
 
 enum TokenTypes {
     NONE,
+    WHITESPACE,
     IDENTIFIER, // variables and all. fn name. 
     NUMBER, // number can be of type integer or float. 
     LITERAL, // strings.
-    OPERATORS, // All different kind of operators. 
     KEYWORD, // the syntax of the language that cannot be used as an Identifier. 
+    // OPERATOR
+
+    ADD, // +, -, *, /, >, < , , ;, 
+    SUB,
+    MUL,
+    DIV,
+    GRT_THN,
+    LSS_THN,
+    EQUAL,
+    R_PAREN,
+    L_PAREN,
+    R_CURL,
+    L_CURL,
+    COMMA,
+    SEM_COM,
+    R_SQBRA,
+    L_SQBRA,
+    UNDER_SC,
+    DASH,
+    AT_RATE,
+    DOLLAR,
+    QUES,
+    STRAIGHT,
+    TILDE,
+    BACK_TICK,
+    HASH,
+    PERCENT,
+    EXCLAIM,
+    CARET,
+    AMPERSAND,
+    BACK_SLASH,
+
+    // These are single and double quotes. 
+    // They will be later decided if they are right or left. 
+    D_QUOTES,
+    S_QUOTES,
+
+    R_DQUOT,
+    L_DQUOT,
+    R_SINQUOT,
+    L_SINQUOT,
 };
 
 
 class Token {
+    public:
+        static std::map<char, TokenTypes> m_symbol_map; 
+        static TokenTypes get_type(char c); 
+
     protected:
         TokenTypes token_type;
         std::string token_value;
+        virtual bool incompatible_type(char next_char) = 0; 
 
     public:
-        Token() {
-        }
+        Token(); 
+        virtual ~Token(); 
 
-        virtual ~Token() {
-        }
+        static void initialize_map(); 
+        const TokenTypes get_type() const; 
 
-        virtual void parse() = 0;
-
+        virtual bool parse(char cur, char next) = 0;
 }; 
 
 class Literal : public Token {
     private:
         int precedence; 
+        static std::set<TokenTypes> m_compatible_types; 
+
+    protected:
+        virtual bool incompatible_type(char next_char) override; 
+        bool double_quot_enabled, single_quot_enabled; 
 
     public:
-        Literal() {
-        }
+        Literal(); 
+        virtual ~Literal(); 
 
-        virtual ~Literal() {
-            std::cout << "Deleted Liter" << std::endl; 
-        }
-
-        virtual void parse() override; 
+        virtual bool parse(char cur, char next) override; 
 };
 
 
 class Identifier : public Token {
     private:
         int precedence; 
+        static std::set<TokenTypes> m_compatible_types; 
+
+
+    protected:
+        virtual bool incompatible_type(char next_char) override; 
 
     public:
-        Identifier() {
-        }
+        Identifier(); 
 
-        virtual ~Identifier() {
-            std::cout << "Deleted Iden" << std::endl; 
-        }
+        virtual ~Identifier(); 
 
-        virtual void parse() override;
+        virtual bool parse(char cur, char next) override;
 };
 
+class Numbers: public Token {
+    private: 
+        static std::set<TokenTypes> m_compatible_types; 
+
+    protected:
+        virtual bool incompatible_type(char next_char) override; 
+
+    public: 
+        Numbers(); 
+        virtual ~Numbers(); 
+
+        virtual bool parse(char cur, char next) override; 
+};
--- src/token.cpp
+++ src/token.cpp
@@ -1,10 +1,104 @@
 #include "../include/token.hpp"
+#include <cassert>
 #include <iostream>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <utility>
+#include "../include/utilities.hpp"
 
-void Literal::parse() {
-    std::cout << "Parse me" << std::endl; 
+std::map<char, TokenTypes> Token::m_symbol_map; 
+
+Token::Token() {
 }
 
-void Identifier::parse() {
-    std::cout << "Parse the identifier" << std::endl; 
+Token::~Token() {
+}
+
+void Token::initialize_map() {
+    POPULATE_MAP(m_symbol_map, '0', '9', TokenTypes::NUMBER);
+    POPULATE_MAP(m_symbol_map, 'a', 'z', TokenTypes::IDENTIFIER);
+    POPULATE_MAP(m_symbol_map, 'A', 'Z', TokenTypes::IDENTIFIER);
+
+    m_symbol_map.insert(std::make_pair('_', TokenTypes::WHITESPACE));
+    m_symbol_map.insert(std::make_pair('\n', TokenTypes::WHITESPACE));
+    m_symbol_map.insert(std::make_pair('\r', TokenTypes::WHITESPACE));
+
+    m_symbol_map.insert(std::make_pair('=', TokenTypes::EQUAL));
+    m_symbol_map.insert(std::make_pair('"', TokenTypes::D_QUOTES));
+    m_symbol_map.insert(std::make_pair('\'', TokenTypes::S_QUOTES));
+}
+
+TokenTypes Token::get_type(char c) {
+    TokenTypes type = NONE;
+
+    try {
+        type = m_symbol_map.at(c); 
+    } catch (std::out_of_range o) {
+        std::cerr << "Character " << c << " is not found " << o.what() << std::endl;  
+        assert(!"Character not found");
+    }
+
+    return type; 
+}
+
+Literal::Literal() {
+
+}
+
+Literal::~Literal() {
+
+}
+
+std::set<TokenTypes> Literal::m_compatible_types; 
+
+bool Literal::parse(char cur, char next) {
+    this->token_value += cur; 
+    return incompatible_type(next); 
+}
+
+// We will need a class seperate for Operators and then we will need to add incompatible_types there, there will be a sub enum for specific operators. 
+bool Literal::incompatible_type(char next_char) {
+    TokenTypes next_type = Token::get_type(next_char); 
+
+    return true;
+}
+
+
+Identifier::Identifier() {
+
+}
+
+Identifier::~Identifier() {
+
+}
+
+std::set<TokenTypes> Identifier::m_compatible_types; 
+
+bool Identifier::parse(char cur, char next) {
+    this->token_value += cur; 
+    return true;
+}
+
+bool Identifier::incompatible_type(char next_char) {
+    return true; 
+}
+
+
+Numbers::Numbers() {
+
+}
+
+Numbers::~Numbers() {
+}
+
+std::set<TokenTypes> Numbers::m_compatible_types; 
+
+bool Numbers::parse(char cur, char next) {
+    this->token_value += cur; 
+    return true; 
+}
+
+bool Numbers::incompatible_type(char next) {
+    return false;
 }
