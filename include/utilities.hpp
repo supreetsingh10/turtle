a9e5c8ab5b17fb1ce962ef199a5fef534a03ab5e
Supreet <singh.supreet14@gmail.com>
2024-05-02 22:34:04 +0530

    Changed a Populate map macro

--- include/utilities.hpp
+++ include/utilities.hpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 #define SAFE_RELEASE_VECTOR(vec) for(auto e : vec) delete e
-#define POPULATE_MAP(m, start, end, type) for(char i = start; i <= end; i++) { m[i] = type; }
+#define POPULATE_MAP(m, start, end, type) for(char i = start; i <= end; i++) { m.insert(std::make_pair(i, type)); }
 #define DEBUG false
 
 namespace Utils {
