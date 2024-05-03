fe7b316d5931b66189b258fdede934b17747a0e5
Supreet <singh.supreet14@gmail.com>
2024-05-04 04:18:43 +0530

    Changed the file reading from source

--- include/source.hpp
+++ include/source.hpp
@@ -1,18 +1,14 @@
 #pragma once
-#include <cstdint>
 #include <fstream>
 #include <string>
 #include <sys/types.h>
 
 class Source {
     public: 
-        Source(): m_line_number(0), m_index(-1) {};
+        Source() {}
         void initialize(const std::string& file_buf); 
         std::fstream& get_file_buffer(); 
 
     protected: 
         std::fstream m_file_buffer; 
-        // index is set to -1 initially; 
-        int m_index, m_seeker; 
-        uint32_t m_line_number; 
 };
